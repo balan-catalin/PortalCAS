3d09ad35b0a8309e6e8c6687cc0b86dca9fc1f95
Balan Catalin <38920688+balan-catalin@users.noreply.github.com>
2019-06-25 18:35:08 +0300

    Add project files.

---
+++ ServiciiREST/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ServiciiREST
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Enabling CORS: https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/enabling-cross-origin-requests-in-web-api
+            config.EnableCors();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Pentru a intoarce datele in format JSON
+            /* config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(
+                config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml")
+            ); */
+        }
+    }
+}
---
+++ ServiciiREST/Controllers/AsiguratController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ServiciiREST.Models;
+
+namespace ServiciiREST.Controllers
+{
+    public class AsiguratController : ApiController
+    {
+        private PortalCasEntities db = new PortalCasEntities();
+
+        // GET: api/Asigurat
+        public IQueryable<Asigurat> GetAsigurat()
+        {
+            return db.Asigurat;
+        }
+
+        // GET: api/Asigurat/5
+        [ResponseType(typeof(Asigurat))]
+        public IHttpActionResult GetAsigurat(string id)
+        {
+            Asigurat asigurat = db.Asigurat.Find(id);
+            if (asigurat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(asigurat);
+        }
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AsiguratExists(string id)
+        {
+            return db.Asigurat.Count(e => e.CNP == id) > 0;
+        }
+    }
+}
